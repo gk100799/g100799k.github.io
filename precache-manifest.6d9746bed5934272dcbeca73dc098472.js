7e810153fa346e26fd718c1789a27eb602b534b6
Gopal Krishna <gopal.krishna@cimpress.com>
2021-05-29 13:48:27 +0530

    Updates

---
+++ precache-manifest.6d9746bed5934272dcbeca73dc098472.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f900ca8f70a510d7563ca5b774734b89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34021645aecac1232a3d",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "12b0151834067ddfdf51",
+    "url": "/static/js/2.57a44e07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57a44e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34021645aecac1232a3d",
+    "url": "/static/js/main.fb393473.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "4ac9099b1dfe081a63e275bbbd7bbe58",
+    "url": "/static/media/ProfilePhoto.4ac9099b.jpg"
+  }
+]);
